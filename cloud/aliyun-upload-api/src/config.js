7712adaafd068073d3c2d13d074a33a481ac15d9
ST-Glow <2117000249@qq.com>
2026-07-18 00:12:04 +0800

    Use the matching Coze OAuth public key id

--- cloud/aliyun-upload-api/src/config.js
+++ cloud/aliyun-upload-api/src/config.js
@@ -20,7 +20,7 @@ function loadConfig(environment = process.env) {
     cozeApiToken: environment.COZE_API_TOKEN || "",
     cozeBotId: "7647799189921284146",
     cozeOauthClientId: environment.COZE_JWT_OAUTH_CLIENT_ID || "1167033540105",
-    cozeOauthPublicKeyId: environment.COZE_JWT_OAUTH_PUBLIC_KEY_ID || "4n-SysxRLNZl_43YXfv9A-M3N2oj0coo-5KJ9pz-aoU",
+    cozeOauthPublicKeyId: environment.COZE_JWT_OAUTH_PUBLIC_KEY_ID || "HzkdmJpFJHuubOaaG3C6bmAjC0oLY6vX2AgtVz5X4ho",
     cozeOauthPrivateKey: environment.COZE_JWT_OAUTH_PRIVATE_KEY || "",
   };
   validateConfig(config);
